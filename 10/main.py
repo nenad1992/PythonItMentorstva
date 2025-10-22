142bc3f3203070444f2a92a21bc417291323bbdd
Nenad <nenadj92@gmail.com>
2025-10-22 21:19:34 +0200

    Finished main Python and data.json

---
+++ 10/main.py
@@ -0,0 +1,17 @@
+import json
+
+# r - read
+with open("data.json", 'r') as file:
+    data = json.load(file)
+    data.append({
+        "name": "Petar Petrovic",
+        "age": 35,
+        "height": 190,
+        "gender": "male"
+    })
+
+print(data)
+
+# w - write
+with open("data.json", 'w') as file:
+    json.dump(data, file, indent=4) # dupm - upisivanje u json fajl, ident=4 - formatiranje data.json fajla
