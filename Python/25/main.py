bb9dd2f81cd72fb29cdfd0bfa3dc3d76c931e11c
Nenad <nenadj92@gmail.com>
2025-11-25 09:54:21 +0100

    25. predavanje

--- Python/25/main.py
+++ Python/25/main.py
@@ -1,6 +1,47 @@
+import re
 import tkinter as tk
 
 window = tk.Tk() # Napravio je novi prozor (window)
-window.geometry("300x100")
+window.geometry("1024x720")
+window.title("Moj program")
+window.configure(bg="black")
+
+# Creating a main label
+tk.Label(window,
+         text="Hello how are you doing?",
+         font=("Arial", 16),
+         fg="dark blue",
+         bg="orange"
+        ).pack()
+
+# Sign Up Button
+def on_click():
+    email = email_entry.get()
+    pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+
+    if re.match(pattern, email) is None:
+        print("Uneti email nije u dobrom formatu")
+
+    if email == "admin@admin.com" and password_entry.get() == "123456":
+        print("Dobrodosao")
+    else:
+        print("Pogresni kredencijali!")
+
+tk.Button(window,
+          text="Sign up!",
+          font=("Arial", 16),
+          bg="dark blue",
+          fg="white",
+          command=on_click
+          ).pack(pady=15)
+
+# Entry
+password_entry = tk.Entry(window, width=50)
+password_entry.pack()
+
+#=========== Email Entry ================
+email_entry = tk.Entry(window, width=50)
+email_entry.pack()
+
 
 window.mainloop()
